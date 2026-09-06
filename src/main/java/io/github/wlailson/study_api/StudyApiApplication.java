137cb312dbf54a6314a24c45812d395b27c4b69f
Wlailson <vlalves4124@gmail.com>
2026-09-06 01:30:26 -0300

    initil commit

---
+++ src/main/java/io/github/wlailson/study_api/StudyApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.wlailson.study_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wlailson/study_api/StudyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wlailson.study_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
