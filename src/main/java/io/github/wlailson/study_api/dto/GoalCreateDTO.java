e46b417be9267219c0d422e40d0848295e3e51ff
Wlailson <vlalves4124@gmail.com>
2026-09-09 01:12:14 -0300

    feat:GoalItem

--- src/main/java/io/github/wlailson/study_api/dto/GoalCreateDTO.java
+++ src/main/java/io/github/wlailson/study_api/dto/GoalCreateDTO.java
@@ -1,7 +1,5 @@
 package io.github.wlailson.study_api.dto;
 
-import io.github.wlailson.study_api.model.Goal;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -11,13 +9,4 @@ public record GoalCreateDTO(
         LocalDate endDate,
         List<GoalItemCreateDTO> items
 ) {
-
-    public GoalCreateDTO(Goal entity) {
-        this(
-                entity.getTitle(),
-                entity.getStartDate(),
-                entity.getEndDate(),
-                entity.getGoalItems().stream().map(GoalItemCreateDTO::new).toList()
-        );
-    }
 }
--- src/main/java/io/github/wlailson/study_api/dto/GoalItemMinDTO.java
+++ src/main/java/io/github/wlailson/study_api/dto/GoalItemMinDTO.java
@@ -2,13 +2,12 @@ package io.github.wlailson.study_api.dto;
 
 import io.github.wlailson.study_api.model.GoalItem;
 
-public record GoalItemDTO(Long id, Long targetInMinutes, SubjectDTO subject) {
+public record GoalItemMinDTO(Long targetInMinutes, String subjectName) {
 
-    public GoalItemDTO(GoalItem entity) {
+    public GoalItemMinDTO(GoalItem entity) {
         this(
-                entity.getId(),
                 entity.getTargetInMinutes(),
-                new SubjectDTO(entity.getSubject())
+                entity.getSubject().getName()
         );
     }
 }
